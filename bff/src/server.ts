6dd56e7efd3d6060d5eb01495c07db419a795f42
nednella <19449811+nednella@users.noreply.github.com>
2026-02-12 21:28:45 +0000

    feat: task 1 bonus solution - replicate redis KV store w/ in-memory hashmap

--- bff/src/server.ts
+++ bff/src/server.ts
@@ -1,6 +1,7 @@
 import express from "express";
 import cors from "cors";
 import { getUserDashboard } from "./service";
+import { DashboardResponse } from "./types";
 
 const app = express();
 const PORT = 3000;
@@ -8,9 +9,29 @@ const PORT = 3000;
 app.use(cors());
 app.use(express.json());
 
+type CacheEntry = {
+    value: DashboardResponse;
+    expires_at: number;
+};
+const cache = new Map<string, CacheEntry>();
+const cacheDurationMs = 60000; // 1 minute
+
 app.get("/api/dashboard/:username", async (req, res) => {
     const { username } = req.params;
+
+    const key = username.trim().toLowerCase();
+    const cached = cache.get(key);
+
+    if (cached && cached.expires_at > Date.now()) {
+        return res.status(200).json(cached.value);
+    }
+
     const dashboard = await getUserDashboard(username);
+    cache.set(key, {
+        value: dashboard,
+        expires_at: Date.now() + cacheDurationMs,
+    });
+
     res.status(200).json(dashboard);
 });
 
