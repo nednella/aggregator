e8a686f879ef7976c24d8bc80c328a80f2a06596
nednella <19449811+nednella@users.noreply.github.com>
2026-02-12 19:21:51 +0000

    feat: initial task 2 solution

--- frontend/src/components/SearchBar.tsx
+++ frontend/src/components/SearchBar.tsx
@@ -1,11 +1,22 @@
+import { useState } from "react";
+
 interface SearchBarProps {
     onSearch: (username: string) => void;
 }
 
 export default function SearchBar({ onSearch: _onSearch }: SearchBarProps) {
+    const [username, setUsername] = useState<string>("");
+
+    const handleSubmit = (e: React.SubmitEvent) => {
+        e.preventDefault();
+        _onSearch(username);
+    };
+
     return (
-        <form className="search-bar" onSubmit={(e) => e.preventDefault()}>
+        <form className="search-bar" onSubmit={handleSubmit}>
             <input
+                value={username}
+                onChange={(e) => setUsername(e.target.value)}
                 className="search-input"
                 type="text"
                 placeholder="Search GitHub username..."
